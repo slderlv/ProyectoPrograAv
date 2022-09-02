e9cd0280255f3708bb777c0a591be1df978cd1a8
Your Name <vicente.briceno@alumnos.ucn.cl>
2022-09-02 17:38:24 -0400

    a

---
+++ Person.py
@@ -0,0 +1,7 @@
+""" Person class"""
+
+class Person:
+  def __init__(self, name, age, dogs) -> None:
+      self.name = name
+      self.age = age
+      self.dogs = dogs
---
+++ main.py
@@ -0,0 +1,9 @@
+from Dog import Dog
+from Person import Person
+
+if __name__ == "__main__":
+    luffy = Dog('Luffy')
+    zoro = Dog('Zoro')
+    nami = Dog('Nami')
+    person = Person('jp',26,[luffy,zoro,nami])
+    
